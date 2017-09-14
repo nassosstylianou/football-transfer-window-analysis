8f89c7bd2c04273a145997412c49aca44b58e8b5
Nassos Stylianou <stylianou.nassos@gmail.com>
2017-09-14 22:21:32 +0100

    removes unused files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {          
+      $.getJSON('data/data.json', function(data) {
+      	$.each(data, function(key, value) {	
+      		$('#list').append('<option value="' + value.club + '">' + value.club + '</option>');
+  		}); 
+	});    
+});
+
+
+function results(selectedTeam) {
+	$.getJSON('data/data.json', function(data) { 
+		$.each(data, function(key, value) {	
+      		if (value.club == selectedTeam) {
+      		var netSpendReply;
+				if (value.net_spend > 0) {
+               netSpendReply =    "Your club spent £" + value.net_spend / 1000000 + " million in this summer's transfer window. They signed a total of " + value.players_in + " players this summer, with " + value.players_out + " players leaving the club." 
+              }    else {
+                 netSpendReply =   "Your club made a profit of £" + Math.abs(value.net_spend / 1000000) + " million in this summer's transfer window."
+              }
+      			$("div").text(netSpendReply);
+      		}
+  		}); 
+	});
+} 
