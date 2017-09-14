404f7ef73a2af294d7d72114ffb77f9cf36b3634
Nassos Stylianou <stylianou.nassos@gmail.com>
2017-09-14 23:33:48 +0100

    adds rounding

--- js/main.js
+++ js/main.js
@@ -12,13 +12,41 @@ function results(selectedTeam) {
 		$.each(data, function(key, value) {	
       		if (value.club == selectedTeam) {
       		var netSpendReply;
-				if (value.net_spend > 0) {
-               netSpendReply =    "Your club spent £" + value.net_spend / 1000000 + " million in this summer's transfer window. They signed a total of " + value.players_in + " players this summer, with " + value.players_out + " players leaving the club." 
-              }    else {
-                 netSpendReply =   "Your club made a profit of £" + Math.abs(value.net_spend / 1000000) + " million in this summer's transfer window."
-              }
-      			$("div").text(netSpendReply);
+				
+        if (value.net_spend == 0) {
+          netSpendReply = value.club + " did not spend any money in this summer's transfer window."
+        } else if (value.net_spend > 0 && value.net_spend < 1000000) {
+            netSpendReply = value.club + " spent £" + Math.round(value.net_spend) + " in this summer's transfer window."
+        } else if (value.net_spend >= 1000000) {
+            netSpendReply = value.club + " spent £" + Math.round(value.net_spend / 1000000) + " million in this summer's transfer window."
+          } else {
+            netSpendReply = "Your club made a profit of £" + Math.round(Math.abs(value.net_spend / 1000000)) + " million in this summer's transfer window."
+          }
+      		
+          $("div").text(netSpendReply);
+
+          var playerNumbersReply = "The club signed a total of " + value.players_in + " players this summer, with " + value.players_out + " players departing.";
+
+          $("#numberOfPlayers").text(playerNumbersReply);
       		}
   		}); 
 	});
 } 
+
+
+
+/*var netSpendReply; 
+if (value.net_spend == 0) {
+  netSpendReply = value.club + " did not spend any money in this summer's transfer window."
+}
+ else if (value.net_spend > 0 && value.net_spend < 1000) {
+      netSpendReply = value.club + " spent £" + value.net_spend + " in this summer's transfer window."
+  }
+ else if (value.net_spend >= 1000 && value.net_spend < 1000000) {
+      netSpendReply = value.club + " spent £" + value.net_spend / 1000 + " thousand in this summer's transfer window."
+  }
+  else if (value.net_spend >= 1000000) {
+   netSpendReply = value.club + " spent £" + value.net_spend / 1000000 + " million in this summer's transfer window."
+              }  
+
+*/
